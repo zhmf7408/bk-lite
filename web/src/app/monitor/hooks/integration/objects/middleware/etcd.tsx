6fe686e7c8cb28129688fbc7d831da7434dc0054
zhongming Fan <zhongmingfan@canway.net>
2026-04-21 16:31:45 +0800

    feature: enhance Etcd monitoring with additional metrics and descriptions

--- web/src/app/monitor/hooks/integration/objects/middleware/etcd.tsx
+++ web/src/app/monitor/hooks/integration/objects/middleware/etcd.tsx
@@ -4,13 +4,17 @@ export const useEtcdBkpullConfig = () => {
     dashboardDisplay: [
       '集群健康',
       '请求性能',
-      '网络',
       '存储',
+      '网络',
+      '运行状态',
     ],
     tableDiaplay: [
       { type: 'enum', key: 'etcd_server_has_leader_gauge' },
+      { type: 'enum', key: 'etcd_server_is_leader_gauge' },
+      { type: 'value', key: 'etcd_backend_quota_usage_percent' },
       { type: 'value', key: 'etcd_mvcc_db_total_size_in_use_in_bytes_gauge' },
-      { type: 'value', key: 'etcd_server_proposals_failed_total_counter_rate' },
+      { type: 'value', key: 'etcd_server_proposals_pending_gauge' },
+      { type: 'value', key: 'etcd_server_slow_apply_total_counter_rate' },
     ],
     groupIds: {},
     collectTypes: {
